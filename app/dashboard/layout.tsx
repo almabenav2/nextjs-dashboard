afa172909e249a453c32f84cb016a464e7d662e8
almabenav2 <almabenavides02@gmail.com>
2024-02-05 18:21:15 -0500

    Fixed Invoices

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
   title: 'Dashboard',
 }; 
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
 }
